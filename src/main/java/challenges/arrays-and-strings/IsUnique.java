1e147303fe0a320628d2180c379eb44dcb468bbc
Sam Genduso <sam.genduso@gmail.com>
2015-08-12 21:57:58 -0600

    first string array challenge with data structures

--- src/main/java/challenges/arrays-and-strings/IsUnique.java
+++ src/main/java/challenges/arrays-and-strings/IsUnique.java
@@ -1,24 +1,32 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
+//learned: StringIndexOutOfBoundsException thrown when index for charAt is negative, too high, or equal to string length
+
 package challenges;
 
-import java.util.ArrayList;
+//without additional data structures:
 
-public class IsUnique {
 
-    public Boolean allUnique(String stringInput) {
-      ArrayList charArray = new ArrayList();
-        for (int i=0; i<stringInput.length(); i++ ) {
-          System.out.println(stringInput.charAt(i));
-          if (charArray.contains(stringInput.charAt(i))) {
-            return false;
-          } else if (i == stringInput.length()-1) {
-            return true;
-          } else {
-            charArray.add(stringInput.charAt(i));
-          }
-        }
-        return true;
-    }
+//using additional data structures:
 
-}
+
+// import java.util.ArrayList;
+//
+// public class IsUnique {
+//
+//     public Boolean allUnique(String stringInput) {
+//       ArrayList charArray = new ArrayList();
+//         for (int i=0; i<stringInput.length(); i++ ) {
+//           System.out.println(stringInput.charAt(i));
+//           if (charArray.contains(stringInput.charAt(i))) {
+//             return false;
+//           } else if (i == stringInput.length()-1) {
+//             return true;
+//           } else {
+//             charArray.add(stringInput.charAt(i));
+//           }
+//         }
+//         return true;
+//     }
+//
+// }
